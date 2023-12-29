86d59a8398506e6a01438ff0e309cd79599b3b98
Donald Roman <romanunuose@gmail.com>
2023-12-29 19:07:29 +0100

    Created icon components

---
+++ src/assets/icons/rightarrow.tsx
@@ -0,0 +1,9 @@
+export default function RightArrow({className} : {className: string}) {
+  return ( 
+    <svg className={className} viewBox="-19.04 0 75.804 75.804" fill="none">
+      <g transform="translate(-831.568 -384.448)">
+        <path d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z"/>
+      </g>
+    </svg>
+  )
+}
